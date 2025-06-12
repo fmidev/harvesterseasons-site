5fd4146785d6368fde5635909eeae3bb0e8be0d8
Tuomo Smolander <58043704+tsmoland@users.noreply.github.com>
2025-06-12 19:30:38 +0300

    SWI2 time dimension from SWI2-M3M3

--- js/anim_smartmet_era5l.js
+++ js/anim_smartmet_era5l.js
@@ -120,7 +120,8 @@ $.get('https://desm.harvesterseasons.com/wms?&service=WMS&request=GetCapabilitie
     let swiDateList;
     for (i = 0; i < layerlist.length; i++) {
         // console.debug(layerlist[i].childNodes[1].firstChild.nodeValue)  
-        if (layerlist[i].childNodes[1].firstChild.nodeValue === 'harvester:swi:SWI1') { // 10.6.2025 Quick fix for missing SWI2 time dimension
+        if (layerlist[i].childNodes[1].firstChild.nodeValue === 'harvester:swi:SWI2-M3M3') {
+        // if (layerlist[i].childNodes[1].firstChild.nodeValue === 'harvester:swi:SWI1') { // 10.6.2025 Quick fix for missing SWI2 time dimension
         // if (layerlist[i].childNodes[1].firstChild.nodeValue === 'harvester:swi:SWI2-0TO1') {
         // if (layerlist[i].childNodes[1].firstChild.nodeValue === 'gui:isobands:SWI_SWI2') {
             // console.debug(layerlist[i].childNodes)
